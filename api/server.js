5ead57be25b3df675ae565e2baeead48713611cb
Sergio Alvarez <salvare736@gmail.com>
2021-05-03 16:18:59 -0400

    GET user by id operation is working

--- api/server.js
+++ api/server.js
@@ -11,12 +11,33 @@ server.get('/api/users', (req, res) => {
     User.find()
         .then(users => {
             // throw new Error('BIG BAD ERROR - BE AFRAID')
-            console.log(users)
+            // console.log(users)
             res.json(users)
         })
         .catch(err => {
             res.status(500).json({
                 error: 'The users information could not be retrieved',
+                message: err.message,
+                stack: err.stack
+            })
+        })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if (!user) {
+                res.status(404).json({
+                    message: `The user with the specified ID, ${req.params.id}, does not exist`
+                })
+            } else {
+                // console.log(user)
+                res.json(user)
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: 'The user information could not be retrieved',
                 message: err.message,
                 stack: err.stack
             })
